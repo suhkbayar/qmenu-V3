5b23994f67157d2f5155ecbc6d75af91fa4cb648
Tsengelbaatar Sukhbayar <suhkbayar@gmail.com>
2023-08-28 18:23:15 +0800

    another changes

--- src/components/Cards/RankingCard.tsx
+++ src/components/Cards/RankingCard.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { RankingMdoal } from '..';
 import { numberFormat } from '../../utils';
-import location from '../../assets/user/location.svg';
 type Props = {
   isrounded: boolean;
   index: number;
   name: string;
   amount: number;
+  configs: any[];
   progress: number;
   price: number;
   image: string;
@@ -14,39 +15,43 @@ type Props = {
   color: string;
 };
 
-const Index = ({ index, name, isrounded, progress, amount, image, price, description, isActive, color }: Props) => {
+const Index = ({ index, isrounded, amount, image, price, configs, description, isActive, color, name }: Props) => {
+  const [visible, setVisible] = useState(false);
+
+  const onShow = () => {
+    setVisible(true);
+  };
+
   return (
     <div className="w-full pt-8 pb-1 ">
-      <div className="flex place-content-between">
-        <span className={`text-xs font-semibold text-current`}>
-          {/* {!isActive && <>{numberFormat.format(amount)} ₮</>} */}
-        </span>
-
-        <span style={{ color: color }} className="text-xs mr-16 font-normal  text-current">
+      <div className="flex place-content-between text-center">
+        <span style={{ color: color }} className="text-xs  font-semibold  w-full text-current">
           {numberFormat.format(price)} ₮
         </span>
       </div>
       <div className="w-full bg-gray-100 relative  dark:bg-gray-700">
         <div style={isrounded ? { left: '-12px' } : {}} className="relative">
-          <div style={{ borderBottomColor: color }} className="drop-shape">
-            <div style={{ backgroundColor: color }} className="circle">
+          <div style={{ borderBottomColor: color }} onClick={() => onShow()} className="drop-shape">
+            <div style={{ backgroundColor: color }} onClick={() => onShow()} className="circle">
               <img className="bagde" src={image} />
             </div>
           </div>
         </div>
         {amount > price ? (
           <div
-            className="rounded-l-md"
+            className={`${index === 1 ? 'rounded-l-md' : ''}`}
             style={{
-              border: `1px solid ${color}`,
+              border: `1px solid #87D068`,
               borderRight: 'none',
             }}
           >
             <div
-              className=" bg-success text-xs font-medium text-blue-100 text-center p-2  rounded-l leading-none "
+              className={`bg-success text-xs font-medium text-blue-100 text-center p-2  ${
+                index === 1 ? 'rounded-l' : ''
+              } leading-none`}
               style={{
                 width: '100%',
-                backgroundColor: `${color}`,
+                backgroundColor: `#87D068`,
               }}
             ></div>
           </div>
@@ -77,7 +82,7 @@ const Index = ({ index, name, isrounded, progress, amount, image, price, descrip
       {isrounded && <span className="text-xs mr-16 font-normal">{description}</span>}
       <div
         className="relative whitespace-nowrap text-end"
-        style={{ width: `${Math.min(100, (Number(amount) / Number(price)) * 100)}%` }}
+        style={{ width: `${Math.min(100, (Number(amount) / Number(price)) * 100) + 10}%` }}
       >
         <span style={{ color: color }} className={`text-xs font-semibold `}>
           {!isActive && (
@@ -87,6 +92,18 @@ const Index = ({ index, name, isrounded, progress, amount, image, price, descrip
           )}
         </span>
       </div>
+      {visible && (
+        <RankingMdoal
+          amount={amount}
+          price={price}
+          visible={visible}
+          configs={configs}
+          description={description}
+          onClose={() => setVisible(false)}
+          name={name}
+          image={image}
+        />
+      )}
     </div>
   );
 };
